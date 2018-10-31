f1f08dd63f659f423e9ab6fd85e59962fdc59127
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-31 10:52:13 -0600

    Toggle experiment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,15 +14,22 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(document).ready(function() {
-  $('[data-js-hide-link]').click(function(event){
-    //$(this).parents('li').hide();
-    $(this).parents('li').fadeOut(2000);
-    event.preventDefault(); 
-  });
+// $(document).ready(function() {
+//   $('[data-js-hide-link]').click(function(event){
+//     //$(this).parents('li').hide();
+//     $(this).parents('li').fadeOut(2000);
+//     event.preventDefault();
+//   });
+//
+//   $('[data-js-search]').change(function(event) {
+//     search_term = $(this).val();
+//     alert('You are searching for ' + search_term);
+//   });
+// });
 
-  $('[data-js-search]').change(function(event) {
-    search_term = $(this).val();
-    alert('You are searching for ' + search_term);
+$(document).ready(function() {
+  $('[data-js-hide-info]').click(function(event){
+    $(this).siblings('li').toggle();
+    event.preventDefault();
   });
 });
